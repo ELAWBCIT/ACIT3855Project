3f37e3dfe5573488b3ff3ec979fecdfb0e16728a
Ernest Law <elawvc@gmail.com>
2025-03-27 15:09:05 -0700

    Cache is stupid

--- dashboard/updateStats.js
+++ dashboard/updateStats.js
@@ -39,8 +39,8 @@ const getStats = () => {
 
 
     makeReq(ANALYZER_API_URL.stats, (result) => {
-        const output = `Station Wait Events: ${result}\n` + 
-                    `Maintenance Inventory Events: ${result}`;
+        const output = `Station Wait Events: ${result.num_station_count}\n` + 
+                    `Maintenance Inventory Events: ${result.num_maintenance_count}`;
 
         document.getElementById("analyzer-stats").innerText = output; 
     });
